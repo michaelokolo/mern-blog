f0563e253d5200797b574fb0aeecd5ae6b74068c
Mike <michaelokolo62@yahoo.com>
2024-02-23 08:22:56 +0000

    complete side bar of the dashboard

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,6 +1,5 @@
 import { useSelector } from 'react-redux';
 import { Outlet, Navigate } from 'react-router-dom';
-import Dashboard from '../pages/Dashboard';
 
 export default function PrivateRoute() {
   const { currentUser } = useSelector((state) => state.user);
