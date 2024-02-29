d2c2d451b5e9c39b3ed84bb276ab96ea77e28481
Mike <michaelokolo62@yahoo.com>
2024-02-29 05:34:39 +0000

    add create  a post api route

---
+++ api/models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        'https://www.growthtribe.com/wp-content/uploads/2018/08/Blog-Post-Feature-Image.jpg',
+    },
+    category: {
+      type: String,
+      default: 'uncategorized',
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
