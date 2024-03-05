3db7f010bfc1421551dbe6480085ab89bc0b08a6
Mike <michaelokolo62@yahoo.com>
2024-03-05 17:42:17 +0000

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,56 @@
-import React from 'react';
-
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3">
+        <div className="">
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Michael's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p className="text-justify">
+              Welcome to Michael's Blog! We're thrilled to have you here. Our
+              blog is a passionate endeavor dedicated to sharing insights,
+              stories, and knowledge on programming. Whether you're a seasoned
+              enthusiast or just starting to explore this fascinating world, our
+              goal is to provide valuable and engaging content that informs,
+              inspires, and entertains.
+            </p>
+            <p className="text-justify">
+              At Michael's Blog, we believe in the power of community and
+              connection. We strive to create a welcoming space where readers
+              can come together to learn, grow, and exchange ideas. Our team of
+              dedicated writers and contributors brings a diverse range of
+              perspectives and expertise, ensuring that there's something for
+              everyone to enjoy.
+            </p>
+            <p className="text-justify">
+              From in-depth guides and tutorials to thought-provoking essays and
+              interviews with industry experts, we cover a wide array of topics
+              to satisfy your curiosity and fuel your passion. We're committed
+              to delivering high-quality content that educates and empowers our
+              readers, helping them navigate the complexities of programming
+              with confidence and clarity.
+            </p>
+            <p className="text-justify">
+              But our blog is more than just a platform for information – it's a
+              community of like-minded individuals who share a common love for
+              programming. We encourage you to join the conversation, share your
+              thoughts, and connect with fellow enthusiasts from around the
+              globe. Together, we can embark on a journey of discovery and
+              growth, enriching our lives and deepening our understanding of
+              programming.
+            </p>
+            <p className="text-justify">
+              Thank you for being a part of our community. Whether you're here
+              to learn, to share, or simply to explore, we're excited to have
+              you with us on this incredible adventure. Here's to a world of
+              endless possibilities and endless learning. Welcome to Michael's
+              Blog – where knowledge meets passion, and curiosity knows no
+              bounds.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import CallToAction from '../components/CallToAction';
 
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div
+      className="min-h-screen max-w-2xl mx-auto flex 
+    justify-center items-center flex-col gap-6 p-3"
+    >
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-justify text-md text-gray-500">
+        Welcome to our Project Page, where we bring the world of web development
+        to life through hands-on projects! Dive into the exciting realms of
+        JavaScript, React, and Next.js as we guide you through engaging
+        tutorials and practical exercises. From building interactive web
+        applications to mastering advanced frontend techniques, our projects
+        offer a dynamic learning experience for developers of all levels. Join
+        us on this journey of discovery and innovation as we harness the power
+        of modern web technologies to create impactful projects that inspire and
+        empower. Let's code, create, and build together!
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
